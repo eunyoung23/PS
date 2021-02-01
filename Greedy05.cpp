6c30a75890cc72cf8d55fce26e7fc243265b65a3
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2021-02-02 00:24:49 +0900

    Add files via upload

---
+++ Greedy05.cpp
@@ -0,0 +1,33 @@
+//
+//  Greedy05.cpp
+//  CodingTest_w
+//
+//  Created by 이은영 on 2021/01/30.
+//
+/*
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> a;
+
+int main(){
+    int N;
+    cin>>N;
+    
+    for(int i=0; i<N; i++){
+        int x;
+        cin>>x;
+        a.push_back(x);
+    }
+
+    sort(a.begin(),a.end());
+    
+    int result=0;
+    
+    cout<<result<<" ";
+    
+    return 0;
+}
+*/
