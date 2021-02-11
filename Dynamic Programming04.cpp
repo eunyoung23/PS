2a02d6c8b5899db86c7f58d27ad4bde11447ce9c
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2021-02-11 23:07:30 +0900

    Update Dynamic Programming04.cpp

--- Dynamic Programming04.cpp
+++ Dynamic Programming04.cpp
@@ -4,15 +4,44 @@
 //
 //  Created by 이은영 on 2021/01/24.
 //
-/*
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
+int n,m;
+vector<int> arr;
+
 int main(){
+    cin>>n>>m;
+    
+    int d[m];
+    d[0]=0;
+    for(int i=1; i<=m; i++){
+        d[i]=10001;
+    }
+    
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    
+    for(int i=0; i<n; i++){
+        int cnt=arr[i];
+        for(int j=cnt; j<=m; j++){
+            if(d[j-cnt]!=10001)
+            d[j]=min(d[j],d[j-cnt]+1);
+        }
+    }
     
     
+    if(d[m]==10001){
+        cout<<-1<<"\n";
+    }else{
+        cout<<d[m]<<"\n";
+    }
     
     return 0;
 }
- */
+ 
