8099aad5ef755fb63eb2dd02a046476e5094881e
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2021-08-25 18:19:58 +0900

    Update 2468.py

--- BOJ/DFS,BFS/2468.py
+++ BOJ/DFS,BFS/2468.py
@@ -28,11 +28,11 @@ def bfs(xPos,yPos,value,visited):
 
 maximum = 0
 for a in range(max): # 비가 안오는 경우인 0부터 max-1까지 조회(max는 모든 지역이 물에 잠기므로 안전영역이 0임 고려할 필요가 없음)
-    visited = [[0]*n for _ in range(n)] # 매 높이마다 조회를 해야하므로 visited를 매번 정의
+    visited = [[0]*n for _ in range(n)] 
     ans=0
     for i in range(n):
         for j in range(n):
-            if graph[i][j] > a and visited[i][j] == 0 : # 비가 온것보다 높은 곳이고 방문하지 않은 경우 bfs호출
+            if graph[i][j] > a and visited[i][j] == 0 : 
                 bfs(i,j,a,visited)
                 ans+=1
     if maximum<ans:
