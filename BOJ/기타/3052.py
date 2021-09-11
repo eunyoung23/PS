d8f1532cfd951c36eb05214fbe834c03ab484539
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2021-09-12 00:31:13 +0900

    Update 3052.py

--- BOJ/기타/3052.py
+++ BOJ/기타/3052.py
@@ -5,3 +5,11 @@ for _ in range(0,10):
     arr.add(N%42)
 
 print(len(arr))
+
+#리스트로 푼거
+num_list = [0 for i in range(42)]
+for i in range(10):
+    num_list[int(input()) % 42] +=1
+
+num_list = [x for x in num_list if x!=0]
+print(len(num_list))
