e9f017cab19f26154863743b8519d887c61cfe51
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-01-20 17:09:01 +0900

    Update 17298.py

--- BOJ/자료구조/17298.py
+++ BOJ/자료구조/17298.py
@@ -1,1 +1,12 @@
 #이중 for문을 사용했더니 시간 복잡도가 O(N제곱)으로 시간초과 판정이 남ㅠㅠ
+N = int(input())
+num = list(map(int,input().split(" ")))
+stack = []
+answer = [-1]*N
+
+for i in range(N):
+  while stack and num[stack[-1]] < num[i]:
+    answer[stack.pop()] = num[i]
+  stack.append(i)
+
+print(*answer)
