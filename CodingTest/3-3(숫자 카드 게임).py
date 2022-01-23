909784d562416d2e47867992b3d7282aad7fa608
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-01-23 16:12:56 +0900

    Update and rename 3-3(숫자 카드 게임) to 3-3(숫자 카드 게임).py

---
+++ CodingTest/3-3(숫자 카드 게임).py
@@ -0,0 +1,23 @@
+# 풀이1
+N,M = map(int,input().split(" "))
+result = 0
+
+for _ in range(N):
+  arr = list(map(int,input().split(" ")))
+  num = min(arr)
+  result = max(num,result)
+
+print(result)
+
+# 풀이2(2중 반복문 구조를 이용하는 답안 예시)
+N,M = map(int,input().split())
+result = 0
+
+for i in range(N):
+  data = list(map(int,input().split()))
+  min_value = 1001
+  for a in data:
+    min_value = min(min_value,a)
+  result = max(min_value,result)
+
+print(result) #최종답안 출력
