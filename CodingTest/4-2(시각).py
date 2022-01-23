a540ee73aeb224e9117bee3ccfd0cac6fbc8ecfc
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-01-24 00:43:37 +0900

    Update 4-2(시각).py

--- CodingTest/4-2(시각).py
+++ CodingTest/4-2(시각).py
@@ -1,2 +1,12 @@
 # 풀이
-N = int(input())
+N =int(input())
+count=0
+
+for i in range(0,N+1):
+  for j in range(0,60):
+    for z in range(0,60):
+      if '3' in str(i)+str(j)+str(z):
+        count+=1
+
+print(count)
+
