88df3ae522ee3aceacdbbcb9f8a2f6e4d89d9786
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-01-27 11:25:19 +0900

    Update 5-1(음료수 얼려 먹기).py

--- CodingTest/5-1(음료수 얼려 먹기).py
+++ CodingTest/5-1(음료수 얼려 먹기).py
@@ -24,3 +24,30 @@ for i in range(N):
       result += 1
 
 print(result)
+
+# 내 풀이
+N,M = map(int,input().split())
+graph = []
+for i in range(N):
+  graph.append(list(map(int,input())))
+
+def dfs(x,y):
+  if x<=-1 or x>=N or y<=-1 or y>=M:
+    return False
+  else:
+    if graph[x][y]==0:
+      graph[x][y]=1
+      dfs(x-1,y)
+      dfs(x,y-1)
+      dfs(x+1,y)
+      dfs(x,y+1)
+      return True
+    return False
+
+result = 0
+for i in range(N):
+  for j in range(M):
+    if dfs(i,j)==True:
+      result+=1
+      
+print(result)
