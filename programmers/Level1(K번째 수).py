a62d71563bee3acbaf8de29adb660fd7ea0e63bb
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-01-30 00:21:41 +0900

    Update Level1(K번째 수).py

--- programmers/Level1(K번째 수).py
+++ programmers/Level1(K번째 수).py
@@ -1,1 +1,15 @@
 # 내 풀이
+def solution(array, commands):
+    i = 0
+    num = len(commands)
+    answer = []
+    while i<num:
+        start = commands[i][0]
+        end = commands[i][1]
+        point = commands[i][2]
+        arr = array[start-1:end]
+        arr.sort()
+        answer.append(arr[point-1])
+        i+=1
+
+    return answer
