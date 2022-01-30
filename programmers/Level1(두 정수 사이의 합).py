09dfd0c13c362f94fdac91ca5f70a338f4e015d7
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-01-30 11:55:13 +0900

    Update Level1(두 정수 사이의 합).py

--- programmers/Level1(두 정수 사이의 합).py
+++ programmers/Level1(두 정수 사이의 합).py
@@ -1,1 +1,19 @@
 # 내 풀이
+def solution(a, b):
+    answer = 0
+    if a<b:
+        arr = [x for x in range(a,b+1)]
+    else:
+        arr = [x for x in range(b,a+1)]
+    answer = sum(arr)
+    return answer
+  
+# 다른 풀이1
+def solution(a, b): 
+    if a>b: a,b = b,a
+    return sum(range(a,b+1))
+
+
+# 다른 풀이2
+def solution(a, b):
+    return (abs(a-b)+1)*(a+b)//2
