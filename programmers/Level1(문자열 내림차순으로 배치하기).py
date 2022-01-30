0adeaa5ec38fb12528d400ff75eb781218c44cd3
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-01-30 15:30:46 +0900

    Update Level1(문자열 내림차순으로 배치하기).py

--- programmers/Level1(문자열 내림차순으로 배치하기).py
+++ programmers/Level1(문자열 내림차순으로 배치하기).py
@@ -1,8 +1,22 @@
 # 내 풀이
+def solution(s):
+    answer = ''
+    arr = []
+    for letter in s:
+        arr.append(ord(letter))
+        
+    arr.sort(reverse=True)
+    for i in arr:
+        answer+=(chr(i))
+    return answer
+  
+  
+# 다른 풀이1
+def solution(s):
+    return ''.join(sorted(s,reverse=True))
 
 
-
-
-
-
-
+# 다른 풀이2
+def solution(s):
+    s = "".join(sorted(s,reverse=True))
+    return s
