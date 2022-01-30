739fecc824e4971c5496689532ad0a3561952bd5
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-01-30 15:41:26 +0900

    Update Level1(문자열 다루기 기본).py

--- programmers/Level1(문자열 다루기 기본).py
+++ programmers/Level1(문자열 다루기 기본).py
@@ -1,20 +1,19 @@
 # 내 풀이
-
-
-
-
+def solution(s):
+    if (len(s)==4 or len(s)==6) and s.isdigit():
+        answer = True
+    else:
+        answer = False
+    return answer
 
 
 # 다른 풀이1
-
-
-
-
-
+def solution(s):
+    return s.isdigit() and len(s) in (4,6)
 
 # 다른 풀이2
-
-
+def solution(s):
+    return s.isdigit() and (len(s)==4 or len(s) == 6)
 
 
 
