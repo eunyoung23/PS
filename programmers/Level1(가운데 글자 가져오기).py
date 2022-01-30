8efb6044f8b75ca3781de1df4a7af349484c40db
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-01-30 12:22:43 +0900

    Update Level1(가운데 글자 가져오기).py

--- programmers/Level1(가운데 글자 가져오기).py
+++ programmers/Level1(가운데 글자 가져오기).py
@@ -1,1 +1,18 @@
 # 내 풀이
+def solution(s):
+    answer = ''
+    num = len(s)
+    if num%2==0:
+        answer = s[(num//2)-1:(num//2)+1]
+    else:
+        answer = s[num//2]
+        
+    return answer
+  
+  
+  # 다른 풀이1
+  def solution(s): 
+    return s[(len(s)-1)//2:len(s)//2+1]
+  
+  
+ 
