e6948b6c8eef7e2467699b8ee3d026ff1b8df89e
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-01-31 23:47:18 +0900

    Create 6-1(위에서 아래로).py

---
+++ CodingTest/6-1(위에서 아래로).py
@@ -0,0 +1,21 @@
+# 내 풀이
+N = int(input())
+arr = []
+for _ in range(N):
+  arr.append(int(input()))
+
+arr.sort(reverse=True)
+
+for i in range(N):
+  print(arr[i],end=' ')
+  
+#풀이1
+N = int(input())
+arr = []
+for _ in range(N):
+  arr.append(int(input()))
+
+arr = sorted(arr,reverse=True)
+
+for i in arr:
+  print(i,end=' ')
