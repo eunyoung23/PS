608db75f5fae564d1b2ec6330fa32e679a56257f
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-02-02 00:30:49 +0900

    Update 6-2(성적이 낮은 순서로 학생 출력하기).py

--- CodingTest/6-2(성적이 낮은 순서로 학생 출력하기).py
+++ CodingTest/6-2(성적이 낮은 순서로 학생 출력하기).py
@@ -1,8 +1,27 @@
-# 내 풀이
+# 내 풀이(딕셔너링형 값 오름차순으로 정렬)
+N = int(input())
 
+dict = {}
+for i in range(N):
+  input_data = input().split(" ")
+  dict[input_data[0]] = input_data[1]
 
+dict = sorted(dict.items(),key = lambda item:item[1])
 
+for score in dict:
+  print(score[1],end=" ")
 
 
 
 # 풀이1
+N = int(input())
+
+array = []
+for i in range(N):
+  input_data = input().split(" ")
+  array.append((input_data[0],int(input_data[1])))
+
+array = sorted(array,key=lambda x: x[1])
+
+for student in array:
+  print(student[0],end=' ')
