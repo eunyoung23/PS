cdc14463c057c4e9f5d473eec51f087cff42dbe2
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-02-03 16:27:22 +0900

    Update 1206번(DFS와 BFS).py

--- BOJ/1206번(DFS와 BFS).py
+++ BOJ/1206번(DFS와 BFS).py
@@ -1,6 +1,16 @@
 # 내 풀이
 N,M,V = map(int,input().split())
+graph = [[0]*(N+1) for _ in range(N+1)]
 
+for _ in range(M):
+  a,b=map(int,input().split())
+  graph[a][b]=1
+  graph[b][a]=1
+
+dfs_visited=[False]*N
+def dfs(graph,v,dfs_visited):
+  dfs_visited[v]=True
+  print(v,end=' ')
 
 
 
