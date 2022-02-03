e8adefe565117a8e4d17af902117c121d199185e
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-02-04 00:44:25 +0900

    DFS/BFS

--- BOJ/1206번(DFS와 BFS).py
+++ BOJ/1206번(DFS와 BFS).py
@@ -1,4 +1,6 @@
 # 내 풀이
+from collections import deque
+
 N,M,V = map(int,input().split())
 graph = [[0]*(N+1) for _ in range(N+1)]
 
@@ -11,7 +13,24 @@ dfs_visited=[False]*N
 def dfs(graph,v,dfs_visited):
   dfs_visited[v]=True
   print(v,end=' ')
+  for w in range(len(graph[v])):
+    if graph[v][w]==1 and dfs_visited[w] == False:
+      dfs(graph,w,dfs_visited)
 
+bfs_visited=[False]*N
+def bfs(graph,start,bfs_visited):
+  queue=deque([start])
+  bfs_visited[start]=True
+  while queue:
+    v = queue.popleft()
+    print(v,end=' ')
+    for w in range(len(graph[v])):
+      if graph[v][w]==1 and bfs_visited[w] == False:
+        queue.append()
+
+
+dfs(graph,1,dfs_visited)
+bfs(graph,1,bfs_visited)
 
 
 
