e538c340e5adac8f801e372df22297b11552d527
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-02-04 18:55:30 +0900

    Update Level1(정수 내림차순으로 배치하기).py

--- programmers/Level1(정수 내림차순으로 배치하기).py
+++ programmers/Level1(정수 내림차순으로 배치하기).py
@@ -1,4 +1,16 @@
 # 내 풀이
-
-
-
+def solution(n):
+    answer = 0
+    n=list(str(n))
+    n.sort(reverse=True)
+    answer=int("".join(n))
+    return answer
+  
+# 풀이1
+def solution(n):
+    answer = ""
+    n=list(str(n))
+    n.sort(reverse=True)
+    for k in n:
+        answer+=str(k)
+    return int(answer)
