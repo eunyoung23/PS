e31d46f4ae957e7b1e5da8dabd6c509ddbb66620
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-02-09 15:55:26 +0900

    Update Level1(문자열 내 마음대로 정렬하기).py

--- programmers/Level1(문자열 내 마음대로 정렬하기).py
+++ programmers/Level1(문자열 내 마음대로 정렬하기).py
@@ -3,10 +3,12 @@ def solution(strings, n):
     return sorted(sorted(strings),key=lambda x : x[n])
 
 # 풀이1
-
-
-
-
-
-
-# 풀이2
+def solution(strings, n):
+    new = []
+    for i in strings:
+        new.append(i[n]+i)
+    new.sort()
+    answer = []
+    for i in new:
+        answer.append(i[1:])
+    return answer
