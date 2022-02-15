407f2774e431aabd9627f681fa4622f4353d8e3f
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-02-15 12:36:32 +0900

    Update and rename kakao 키패드 누르기.py to Level1(kakao 키패드 누르기).py

--- programmers/Level1(kakao 키패드 누르기).py
+++ programmers/Level1(kakao 키패드 누르기).py
@@ -48,3 +48,49 @@ def solution(numbers, hand):
                     right_pos = str(num)
 
     return answer
+
+
+# 내 풀이
+def distance(hand,cos):
+    phone={
+        "1":(0,0),"2":(1,0),"3":(2,0),
+        "4":(0,1),"5":(1,1),"6":(2,1),
+        "7":(0,2),"8":(1,2),"9":(2,2),
+        "*":(0,3),"0":(1,3),"#":(2,3)
+    }
+    
+    distance=abs(phone[hand][0]-phone[cos][0])+abs(phone[hand][1]-phone[cos][1])
+    
+    return distance
+
+
+def solution(numbers, hand):
+    answer = ''
+    left_hand="*"
+    right_hand="#"
+    
+    for num in numbers:
+        if num in [1,4,7]:
+            answer+="L"
+            left_hand=str(num)
+        elif num in [3,6,9]:
+            answer+="R"
+            right_hand=str(num)
+        else:
+            left_distance=distance(left_hand,str(num))
+            right_distance=distance(right_hand,str(num))
+            if left_distance<right_distance:
+                answer+="L"
+                left_hand=str(num)
+            elif left_distance>right_distance:
+                answer+="R"
+                right_hand=str(num)
+            else:
+                if hand == "right":
+                    answer+="R" 
+                    right_hand=str(num)
+                elif hand == "left":
+                    answer+="L"
+                    left_hand=str(num)
+    
+    return answer
