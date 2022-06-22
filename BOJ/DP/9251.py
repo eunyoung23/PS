37d5c0369f95e52966c034797a94daecbf023cd6
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-06-22 15:04:38 +0900

    Update and rename 9252.py to 9251.py

--- BOJ/DP/9251.py
+++ BOJ/DP/9251.py
@@ -1,20 +1,18 @@
 import sys
-
 input=sys.stdin.readline
 
 str1=input().rstrip()
 str2=input().rstrip()
-result=[]
 
 len1=len(str1)
 len2=len(str2)
-data=[[0 for _ in range(len2+1)]for _ in range(len1+1)]
+cache=[[0]*(len2+1)for _ in range(len1+1)]
 
 for i in range(1,len1+1):
     for j in range(1,len2+1):
         if str1[i-1]==str2[j-1]:
-            data[i][j]=data[i-1][j-1]+1
+            cache[i][j]=cache[i-1][j-1]+1
         else:
-            data[i][j]=max(data[i][j-1],data[i-1][j])
+            cache[i][j]=max(cache[i][j-1],cache[i-1][j])
 
-print(data[len1][len2])
+print(cache[-1][-1])
