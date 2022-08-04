542d064981a5f68add56cac462135b7cd7811adb
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-08-04 21:59:32 +0900

    Update Level1(2016년).py

--- programmers/Level1(2016년).py
+++ programmers/Level1(2016년).py
@@ -1,1 +1,16 @@
 # 내 풀이
+def solution(a, b):
+    answer = ''
+    cal = 0
+
+    days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    week = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
+
+    for i in range(0, a - 1):
+        cal += days[i]
+
+    cal += b
+
+    answer = week[cal % 7]
+
+    return answer
