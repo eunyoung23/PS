f8b2d33693a7bf27e1832ef99031e739d9742be0
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-08-09 23:00:32 +0900

    Update Level1(핸드폰 번호 가리기).py

--- programmers/Level1(핸드폰 번호 가리기).py
+++ programmers/Level1(핸드폰 번호 가리기).py
@@ -1,1 +1,29 @@
+#case1
+def solution(phone_number):
+    num = len(phone_number[:-4])
+    for i in range(num):
+        phone_number=phone_number.replace(phone_number[i],"*",1)
+    return phone_number
 
+#case2
+def solution(phone_number):
+    answer = ''
+
+    for i in range(0, len(phone_number) - 4):
+        answer += "*"
+
+    answer += phone_number[-4:]
+
+    return answer
+
+
+#case3
+def solution(phone_number):
+    answer = ''
+
+    for num in range(len(phone_number[:-4])):
+        answer += "*"
+
+    answer += phone_number[-4:]
+
+    return answer
