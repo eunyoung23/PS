d49f7c0c404fc085157025a7429d89ee92964f95
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-08-12 00:01:05 +0900

    Update 48. Rotate Image.py

--- LeetCode/48. Rotate Image.py
+++ LeetCode/48. Rotate Image.py
@@ -28,3 +28,15 @@ class Solution:
                 tmp=matrix[i][j]
                 matrix[i][j]=matrix[j][i]
                 matrix[j][i]=tmp
+                
+#case3
+class Solution:
+    def rotate(self, matrix: List[List[int]]):
+        n=len(matrix)
+        for i in range(0,n//2):
+            for j in range(n):
+                matrix[i][j],matrix[n-i-1][j]=matrix[n-i-1][j],matrix[i][j]
+                  
+        for i in range(0,n):
+            for j in range(i,n):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
