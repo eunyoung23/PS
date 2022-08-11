68b4ff02d026e7130b0aa4cf2730ae3342e84bfa
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-08-11 23:01:51 +0900

    Update 48. Rotate Image.java

--- LeetCode/48. Rotate Image.java
+++ LeetCode/48. Rotate Image.java
@@ -1,3 +1,4 @@
+#case1
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
@@ -11,5 +12,31 @@ class Solution {
                 matrix[j][n-i-1]=t;
             }
         }
+    }
+}
+
+#case2
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        
+        //reverse up and down
+        for(int i=0; i<n/2; i++){
+            for(int j=0; j<n; j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[n-1-i][j];
+                matrix[n-i-1][j]=temp;
+            }
+        }
+        
+        //reverse diagonally
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        
     }
 }
