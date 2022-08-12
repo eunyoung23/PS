5b041366d3e1eddd905f11fb8b554f2d2aebe8dc
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-08-12 22:33:57 +0900

    Update Level1(kakao 키패드 누르기).py

--- programmers/Level1(kakao 키패드 누르기).py
+++ programmers/Level1(kakao 키패드 누르기).py
@@ -1,3 +1,4 @@
+#1
 def solution(numbers, hand):
     answer = ''
    
@@ -63,7 +64,7 @@ def distance(hand,cos):
     
     return distance
 
-
+#2
 def solution(numbers, hand):
     answer = ''
     left_hand="*"
@@ -92,5 +93,52 @@ def solution(numbers, hand):
                 elif hand == "left":
                     answer+="L"
                     left_hand=str(num)
+    
+    return answer
+
+#3
+#0 - 키보드를 좌표로 어떻게 나타낼지
+#1 - number를 반복문을 돌림
+#2 - 현재 위치에서 거리를 계산함
+#3 - 둘 비교해서 작은 값을 넣어줌, 같으면 왼손잡이이면 왼쪽, 오른손잡이이면 오르쪽 넣어줌
+#4 - 왼손과 오른손의 위치 바꿔줌
+
+def solution(numbers, hand):
+    answer = ""
+    
+    phone={
+        "1":(0,0),"2":(0,1),"3":(0,2),
+        "4":(1,0),"5":(1,1),"6":(1,2),
+        "7":(2,0),"8":(2,1),"9":(2,2),
+        "*":(3,0),"0":(3,1),"#":(3,2)
+    }
+    
+    lhand="*"
+    rhand="#"
+    
+    for num in numbers:
+        num=str(num)
+        if num=="1" or num=="4" or num=="7":
+            answer+="L"
+            lhand=num
+        elif num=="3" or num=="6" or num=="9":
+            answer+="R"
+            rhand=num
+        else:
+            ldis=abs(phone[lhand][0]-phone[num][0])+abs(phone[lhand][1]-phone[num][1])
+            rdis=abs(phone[rhand][0]-phone[num][0])+abs(phone[rhand][1]-phone[num][1])
+            if ldis==rdis:
+                if hand=="right":
+                    answer+="R"
+                    rhand=num
+                else:
+                    answer+="L"
+                    lhand=num
+            elif ldis>rdis:
+                answer+="R"
+                rhand=num
+            else:
+                answer+="L"
+                lhand=num
     
     return answer
