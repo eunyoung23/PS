5019a03ba594069aad5bb57381c4302abf3aa297
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-08-13 23:47:01 +0900

    Update Level1(비밀지도).py

--- programmers/KAKAO/Level1(비밀지도).py
+++ programmers/KAKAO/Level1(비밀지도).py
@@ -1,1 +1,11 @@
-
+#비트연산에 대해 생각을 못해서 블로그 참고함
+def solution(n, arr1, arr2):
+    answer = []
+    
+    for i in range(n):
+        tmp=bin(arr1[i]|arr2[i])
+        tmp=tmp[2:].zfill(n)
+        tmp=tmp.replace('1','#').replace('0',' ')
+        answer.append(tmp)
+        
+    return answer
