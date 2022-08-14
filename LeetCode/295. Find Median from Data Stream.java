62c023012c8993c0d0f52f28be0bf07da39bd9d6
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-08-14 16:22:29 +0900

    Update 295. Find Median from Data Stream.java

--- LeetCode/295. Find Median from Data Stream.java
+++ LeetCode/295. Find Median from Data Stream.java
@@ -1,1 +1,25 @@
-
+class MedianFinder {
+    
+    PriorityQueue<Integer> high=new PriorityQueue<>((a,b)-> b-a);
+    PriorityQueue<Integer> low=new PriorityQueue<>();
+    boolean even=true;
+    
+    public MedianFinder() {
+        
+    }
+    
+    public void addNum(int num) {
+        if(even){
+            high.add(num);
+            low.add(high.poll());
+        }else{
+            low.add(num);
+            high.add(low.poll());
+        }
+        even = !even;
+    }
+    
+    public double findMedian() {
+        return even ? (low.peek()+high.peek())/2.0 : low.peek();
+    }
+}
