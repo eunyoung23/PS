e0f6116b6e3a5eca08e57bb48a3b3beed6e37dfc
eunyoung23 <70764912+eunyoung23@users.noreply.github.com>
2022-08-20 12:34:58 +0900

    Update 2309- 일곱 난쟁이.py

--- BOJ/정렬/2309- 일곱 난쟁이.py
+++ BOJ/정렬/2309- 일곱 난쟁이.py
@@ -1,25 +1,31 @@
+#2중 for문 안에 if문이 있을때 break문
+# 19번째의 break문은 j에 대한 for문을 탈출하기 ㅜ이해서 사용됨.따라서 i에 대한 for문에서 아직 탈출하지 못했기 때문에 리스트에 7명이 남았음에도 불구하고 계속 돌아감
+# 따라서 20,21번 줄 처리 또는 flag 처리를 꼭 해줘야 함.
+# 실제 반레:
+# Input : [1,9,2,8,3,7,4,6,70]
+# Output : [2,4,6,8,70]
+# Answer : [2,3,4,5,6,7,8,70]
 import sys
 
 input=sys.stdin.readline
 
-arr=[]
+height=[]
 
 for _ in range(9):
-  arr.append(int(input()))
+    height.append(int(input()))
 
-arr.sort()
+for i in range(0,len(height)-1):
+    for j in range(i+1,len(height)):
+        num1=height[i]
+        num2=height[j]
+        if sum(height)-(num1+num2)==100:
+            height.remove(num1)
+            height.remove(num2)
+            break
+    if len(height)<9:
+        break
 
-for i in range(len(arr)-1):
-  for j in range(i+1,len(arr)):
-    if sum(arr)-arr[i]-arr[j]==100:
-      n1=arr[i]
-      n2=arr[j]
-      arr.remove(n1)
-      arr.remove(n2)
-      break
-    else:
-      pass
+height.sort()
 
-
-for num in arr:
-  print(num)
+for num in height:
+    print(num)
